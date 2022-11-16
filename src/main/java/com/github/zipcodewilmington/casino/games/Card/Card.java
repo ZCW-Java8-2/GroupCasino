f8820a255e1fd7a7b2b9e33ea40886bba356b10d
Eryk <eryk@Zipcoders-MacBook-Pro-71.local>
2022-11-16 13:26:33 -0500

    add card class methods

--- src/main/java/com/github/zipcodewilmington/casino/games/Card/Card.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/Card/Card.java
@@ -1,4 +1,40 @@
 package com.github.zipcodewilmington.casino.games.Card;
 
 public class Card {
+private Suits suit;
+private PrintedValues printedValues;
+private boolean faceUp = false;
+
+    public Card(Suits suit, PrintedValues printedValues, boolean faceUp) {
+        this.suit = suit;
+        this.printedValues = printedValues;
+        this.faceUp = faceUp;
+    }
+
+    public Suits getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suits suit) {
+        this.suit = suit;
+    }
+
+    public PrintedValues getPrintedValues() {
+        return printedValues;
+    }
+
+    public void setPrintedValues(PrintedValues printedValues) {
+        this.printedValues = printedValues;
+    }
+
+    public boolean isFaceUp() {
+        return faceUp;
+    }
+
+    public void setFaceUp(boolean faceUp) {
+        this.faceUp = faceUp;
+    }
+    public void flipCard(){
+        this.faceUp = !faceUp; 
+    }
 }
