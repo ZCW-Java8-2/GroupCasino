7630ed48f187a972d199edeea451bb4c8280d03d
John <john@Zipcoders-MacBook-Pro-46.local>
2022-11-16 13:31:32 -0500

    created abstract

--- src/main/java/com/github/zipcodewilmington/casino/CasinoAccountManager.java
+++ src/main/java/com/github/zipcodewilmington/casino/CasinoAccountManager.java
@@ -6,6 +6,7 @@ package com.github.zipcodewilmington.casino;
  * it is advised that every instruction in this class is logged
  */
 public class CasinoAccountManager {
+
     /**
      * @param accountName     name of account to be returned
      * @param accountPassword password of account to be returned
@@ -16,6 +17,8 @@ public class CasinoAccountManager {
         String currentClassName = getClass().getName();
         String errorMessage = "Method with name [ %s ], defined in class with name [ %s ] has  not yet been implemented";
         throw new RuntimeException(String.format(errorMessage, currentMethodName, currentClassName));
+
+
     }
 
     /**
@@ -30,6 +33,7 @@ public class CasinoAccountManager {
         String currentClassName = getClass().getName();
         String errorMessage = "Method with name [ %s ], defined in class with name [ %s ] has  not yet been implemented";
         throw new RuntimeException(String.format(errorMessage, currentMethodName, currentClassName));
+        
     }
 
     /**
