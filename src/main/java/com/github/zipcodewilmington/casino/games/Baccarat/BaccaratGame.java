e185944418ef68d93c7b545e64e1710e61b38060
tmcorbin96 <115915843+tmcorbin96@users.noreply.github.com>
2022-11-17 08:03:11 -0500

    baccarat and slots maybe filled?

--- src/main/java/com/github/zipcodewilmington/casino/games/Baccarat/BaccaratGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/Baccarat/BaccaratGame.java
@@ -1,4 +1,49 @@
 package com.github.zipcodewilmington.casino.games.Baccarat;
-
+import com.github.zipcodewilmington.casino.gameobjects.DeckOfCards;
+import com.github.zipcodewilmington.casino.gameobjects.SingleCard;
+import com.github.zipcodewilmington.utils.AnsiColor;
+import com.github.zipcodewilmington.utils.IOConsole;
 public class BaccaratGame {
+    private final IOConsole console = new IOConsole(AnsiColor.CYAN);
+    SingleCard[][] bDeck = new DeckOfCards().deck;
+
+
+    int dealerValue;
+    int playerValue;
+    public BaccaratGame () { }
+
+
+
+
+    private void ShuffleCards() {
+//        for (int i = 0; i < bDeck.length; i++) {
+//            int j = (int) (Math.random() * bDeck.length);
+//            SingleCard[] shuffled = bDeck[i];
+//            bDeck[i] = bDeck[j];
+//            bDeck[j] = shuffled;
+//        }
+    }
+
+    private void IsTie () {
+       // if (dealerValue == playerValue) {
+       //     return ;
+       // }
+
+    }
+
+    private boolean NextRound () {
+        return false;
+    }
+
+     private void AddPoints () {
+
+     }
+
+     private void LosePoints () {
+
+     }
+
+     private boolean IsHandEmpty () {
+        return false;
+     }
 }
