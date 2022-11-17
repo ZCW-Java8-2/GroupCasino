59a40556830f8e21e6ded185bec5334bcbd3ac1d
thienle77 <tle9677@gmail.com>
2022-11-17 16:22:16 -0500

    SlotsGame completed

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -1,76 +1,49 @@
 package com.github.zipcodewilmington.casino.games.slots;
 
 import com.github.zipcodewilmington.casino.games.GameTypes.RandomGame;
+
+import java.util.Arrays;
 import java.util.Random;
-import java.util.Scanner;
 
-/**
- * Created by leon on 7/21/2020.
- */
+
 public class SlotsGame extends RandomGame {
-    //test comment
 
+   public int[] arr = new int[3];
+//
+//    public int[] getArr() {
+//        return arr;
+//    }
+//
+//    public void setArr(int[] arr) {
+//        this.arr = arr;
+//    }
+public SlotsGame(){
+    super();
+}
+   // SlotsGame slot = new SlotsGame();
+    public int[] slotMachine() {
+       Random generator = new Random();
+       // int slot = (int) (Math.random() * 7) + 0;
 
-   // public static void main(String[] args[]) {
-   public SlotsGame(int){
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-
-        int input;
-        int Chips = 50;
-        int Slot1, Slot2, Slot3;
-
-        do {
-        System.out.println("Slot Machine");
-        System.out.println("Chips" + Chips);
-        System.out.println("Press Y to play, press N to quit the game");
-
-        input = scanner.nextInt();
-
-        public static int ()
-            Slot1 = random.nextInt(5) + 1;
-            Slot2 = random.nextInt(5) + 1;
-            Slot3 = random.nextInt(5) + 1;
-            // slots range is 5 and starts at 1
-            System.out.println(Slot1 + " " + Slot2 + " " + Slot3);
-
-            if (Slot1 == Slot2 && Slot1 == Slot3) {
-                Chips += 100;
-                System.out.println("Congrats, you won 100 chips!");
-            } else if (Slot1 == Slot2 || Slot1 == Slot3 || Slot2 == Slot3) {
-                Chips += 50;
-                System.out.println("You won 50 chips");
-            } else {
-                System.out.println("Sorry you lose");
-                Chips -= 20;
-
+//       int[] arr = new int[3];
+       for (int i = 0; i < arr.length; i++) {
+           arr[i] = generator.nextInt(7);
+       }
+       return arr;
+   }
+    public String pullLever() {
+//        boolean playGame = true;
+       // for (int i = 0; i < arr.length; i++) {
+        int count =0;
+        while(count<arr.length-1){
+        if (arr[count]!=arr[count+1]){
+                return "You lose!";
+            }else{
+            count++;
             }
-        } while ('Y'); // as long as input is Y then game will continue
+
+        }
+        return "You win!";
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
