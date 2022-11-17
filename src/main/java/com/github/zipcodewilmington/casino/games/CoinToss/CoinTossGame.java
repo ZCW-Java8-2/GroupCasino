4f518f7e8fc3222ff96ea88bdc55b8e9d3205124
Calvin <calvin@Zipcoders-MacBook-Pro.local>
2022-11-16 20:06:22 -0500

    complete CoinToss game

--- src/main/java/com/github/zipcodewilmington/casino/games/CoinToss/CoinTossGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/CoinToss/CoinTossGame.java
@@ -1,4 +1,31 @@
 package com.github.zipcodewilmington.casino.games.CoinToss;
+import com.github.zipcodewilmington.casino.gameobjects.RNG_ITEM;
+import com.github.zipcodewilmington.utils.AnsiColor;
+import com.github.zipcodewilmington.utils.IOConsole;
 
 public class CoinTossGame {
+    private final IOConsole console = new IOConsole(AnsiColor.BLUE);
+
+    public CoinTossGame() {
+
+        RNG_ITEM coin = new RNG_ITEM(2);
+
+        int playerGuess = console.getIntegerInput("1. Heads" +
+                                                        "\n2. Tails" +
+                                                        "\nEnter a number:");
+        int flipResult = coin.runRNG();
+
+        if (isHeadsOrTails(playerGuess, flipResult)) {
+            System.out.println("Congrats! You won the coin toss!");
+        }
+        else {
+            System.out.println("Game over! You lost the coin toss!");
+        }
+    }
+
+    public boolean isHeadsOrTails(int playerGuess, int flipResult) {
+        if(playerGuess == flipResult) { return true; }
+            return false;
+    }
+
 }
