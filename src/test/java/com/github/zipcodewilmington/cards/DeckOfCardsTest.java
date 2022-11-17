f4f1a978c3e8a45b1fda7b7627ddd6ca12882cd6
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-11-16 21:17:33 -0500

    created test class for deck of cards

---
+++ src/test/java/com/github/zipcodewilmington/cards/DeckOfCardsTest.java
@@ -0,0 +1,14 @@
+package com.github.zipcodewilmington.cards;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DeckOfCardsTest {
+
+    @Test
+    public void drawRandomCardTest(){
+
+    }
+
+}
