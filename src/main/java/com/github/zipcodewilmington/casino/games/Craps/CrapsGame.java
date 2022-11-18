c481d8226e4730dec638cc5bb06e15f75ff664ac
tmcorbin96 <115915843+tmcorbin96@users.noreply.github.com>
2022-11-17 21:43:00 -0500

    miserable

--- src/main/java/com/github/zipcodewilmington/casino/games/Craps/CrapsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/Craps/CrapsGame.java
@@ -1,4 +1,42 @@
 package com.github.zipcodewilmington.casino.games.Craps;
 
+import com.github.zipcodewilmington.casino.gameobjects.RNG_ITEM;
+import com.github.zipcodewilmington.utils.AnsiColor;
+import com.github.zipcodewilmington.utils.IOConsole;
+
 public class CrapsGame {
+
+    private  final IOConsole console = new IOConsole(AnsiColor.AUTO);
+    RNG_ITEM dice = new RNG_ITEM(12);
+    // RNG_ITEM dice2 = new RNG_ITEM(6);
+    public void announceGame() {console.println("Welcome to Craps!");
+    }
+
+    public void WelcomeToCraps() {
+    }
+    public Integer getFirstRoll(){
+        return console.getIntegerInput("Throw the Dice!" +
+                "\n1. Throw"
+        );
+    }
+    public int rollDice() {return dice.runRNG();
+    }
+    //public int rollDice2() {return dice2.runRNG();
+    //}
+
+    public boolean doesItMatch(int firstRoll, int rollResult) {
+        return firstRoll == rollResult;
+    }
+    public boolean isSeven( int rollResult) {
+        return rollResult == 7;
+    }
+    public String resultMessage (int firstRoll, int rollResult) {
+        if (doesItMatch(firstRoll, rollResult)) {
+            return "Congrats! You Win!";
+        } else if (firstRoll != rollResult) {
+            return "Try Again";
+        } else if (isSeven(rollResult)) {
+
+        }return "You Lose!";
+    }
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/Craps/CrapsPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/Craps/CrapsPlayer.java
@@ -1,4 +1,26 @@
 package com.github.zipcodewilmington.casino.games.Craps;
 
-public class CrapsPlayer {
+import com.github.zipcodewilmington.casino.GamblerInterface;
+import com.github.zipcodewilmington.casino.Player;
+import com.github.zipcodewilmington.casino.PlayerInterface;
+import com.github.zipcodewilmington.casino.casinoaccount.CasinoAccount;
+
+public class CrapsPlayer extends Player implements PlayerInterface, GamblerInterface {
+    public void placeBet() {
+    }
+
+    @Override
+    public void increaseBet() {
+
+    }
+
+    @Override
+    public CasinoAccount getCasinoAccount() {
+        return null;
+    }
+
+    @Override
+    public <SomeReturnType> SomeReturnType play() {
+        return null;
+    }
 }
