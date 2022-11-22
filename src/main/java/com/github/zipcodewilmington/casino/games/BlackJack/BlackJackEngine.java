7512f601e76789b7b60b3d3f68af94aa6bdffa92
Carolina Diazgranados <linacello@gmail.com>
2022-11-21 22:19:58 -0500

    working on BlackJack

--- src/main/java/com/github/zipcodewilmington/casino/games/BlackJack/BlackJackEngine.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/BlackJack/BlackJackEngine.java
@@ -1,6 +1,53 @@
 package com.github.zipcodewilmington.casino.games.BlackJack;
 
+import com.github.zipcodewilmington.casino.casinoaccount.CasinoAccount;
+import com.github.zipcodewilmington.casino.gameobjects.DeckOfCards;
+import com.github.zipcodewilmington.casino.gameobjects.SingleCard;
+import com.github.zipcodewilmington.utils.AnsiColor;
+import com.github.zipcodewilmington.utils.IOConsole;
+
+import java.io.Console;
+import java.util.ArrayList;
+
 public class BlackJackEngine {
+    BlackJackGame game = new BlackJackGame();
+    private static IOConsole console = new IOConsole(AnsiColor.YELLOW);
+//    public void run(CasinoAccount activeAccount) {
+//        DeckOfCards deck = new DeckOfCards();
+//        deck.drawRandomCard();
+//    }
+public static void main(String[] args) {
+    DeckOfCards deck = new DeckOfCards();
+    BlackJackGame game = new BlackJackGame();
+    IOConsole console = new IOConsole(AnsiColor.YELLOW);
+    //create dealer and player hands
+    ArrayList<SingleCard> dealerHand = new ArrayList<>();
+    ArrayList<SingleCard> playerHand = new ArrayList<>();
+    int dealerSum = 0;
+    int playerSum = 0;
+
+    //draw 4 initial cards
+    for(int i = 0; i < 2; i++) {
+        dealerHand.add(deck.drawRandomCard());
+        playerHand.add(deck.drawRandomCard());
+    }
+
+    console.println(String.valueOf(dealerHand)); //test
+    console.println(String.valueOf(playerHand)); //test
+
+    //add values
+    for(int i = 0; i < dealerHand.size(); i++) {
+        dealerSum += game.getFaceValue(dealerHand.get(i));
+        playerSum += game.getFaceValue(playerHand.get(i));
+    }
+    //isPlayerAce();
+    console.println(String.valueOf(dealerSum));
+    console.println(String.valueOf(playerSum));
+
+
+
+}
+
 
 
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/BlackJack/BlackJackGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/BlackJack/BlackJackGame.java
@@ -1,11 +1,21 @@
 package com.github.zipcodewilmington.casino.games.BlackJack;
 
-public class BlackJackGame {
+import com.github.zipcodewilmington.casino.GamblingInterface;
+import com.github.zipcodewilmington.casino.GameInterface;
+import com.github.zipcodewilmington.casino.PlayerInterface;
+import com.github.zipcodewilmington.casino.gameobjects.CardDeckFaceEnum;
+import com.github.zipcodewilmington.casino.gameobjects.DeckOfCards;
+import com.github.zipcodewilmington.casino.gameobjects.SingleCard;
+import com.github.zipcodewilmington.utils.AnsiColor;
+import com.github.zipcodewilmington.utils.IOConsole;
 
+public class BlackJackGame implements GamblingInterface, GameInterface {
     private int playerScoreSum, dealerScoreSum;
+    IOConsole console = new IOConsole(AnsiColor.YELLOW);
 
-    //TODO create cards/deck
-//    private ArrayList<Cards> deck;
+
+
+
 
     //TODO create card
 //    private Card dealerInitialCard;
@@ -24,6 +34,43 @@ public class BlackJackGame {
 
     public int flipDealersCard(){
         return 0;
+    }
+
+    public int getFaceValue(SingleCard card) {
+        if (CardDeckFaceEnum.TWO.equals(card.getFace())) {
+            return 2;
+        } else if (CardDeckFaceEnum.THREE.equals(card.getFace())) {
+            return 3;
+        } else if (CardDeckFaceEnum.FOUR.equals(card.getFace())) {
+            return 4;
+        } else if (CardDeckFaceEnum.FIVE.equals(card.getFace())) {
+            return 5;
+        } else if (CardDeckFaceEnum.SIX.equals(card.getFace())) {
+            return 6;
+        } else if (CardDeckFaceEnum.SEVEN.equals(card.getFace())) {
+            return 7;
+        } else if (CardDeckFaceEnum.EIGHT.equals(card.getFace())) {
+            return 8;
+        } else if (CardDeckFaceEnum.NINE.equals(card.getFace())) {
+            return 9;
+        } else if (CardDeckFaceEnum.TEN.equals(card.getFace())) {
+            return 10;
+        } else if (CardDeckFaceEnum.JACK.equals(card.getFace())) {
+            return 10;
+        } else if (CardDeckFaceEnum.QUEEN.equals(card.getFace())) {
+            return 10;
+        } else if (CardDeckFaceEnum.KING.equals(card.getFace())) {
+            return 10;
+        } else if (CardDeckFaceEnum.ACE.equals(card.getFace())) {
+            return 1; //plus 10 on Engine
+        }
+        return 0;
+    }
+
+    public int isPlayerAce(SingleCard card) {
+        if (CardDeckFaceEnum.ACE.equals(card.getFace())) {
+            return console.getIntegerInput("You drew an ace! Would you like the value to be 1 or 11?").toLowerCase();
+        }
     }
 
     public String findWinner(int playerScoreSum, int dealerScoreSum) {
@@ -49,5 +96,70 @@ public class BlackJackGame {
 
     public boolean isGreaterThan21(int playerScoreSum){
         return playerScoreSum > 21; // returns true if
+    }
+
+    @Override
+    public double winBet(double bet, double balance) {
+        return 0;
+    }
+
+    @Override
+    public double loseBet(double bet, double balance) {
+        return 0;
+    }
+
+    @Override
+    public void addToBalance() {
+
+    }
+
+    @Override
+    public void subFromBalance() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void gameName() {
+
+    }
+
+    @Override
+    public void gameRules() {
+
+    }
+
+    @Override
+    public void add(PlayerInterface player) {
+
+    }
+
+    @Override
+    public void remove(PlayerInterface player) {
+
+    }
+
+    @Override
+    public void isGameOver() {
+
+    }
+
+    @Override
+    public boolean evaluateTurn() {
+        return false;
+    }
+
+    @Override
+    public void playAgain() {
+
+    }
+
+    @Override
+    public void exitCasino() {
+
     }
 }
